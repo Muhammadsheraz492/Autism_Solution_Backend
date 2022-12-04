56d51d3c4b5fcee4c0316c3ab218ada0f9cabbae
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2022-12-04 12:34:04 +0500

    okey working

--- Api/Routes/Product.js
+++ Api/Routes/Product.js
@@ -2,6 +2,17 @@ const express=require('express');
 const { default: mongoose } = require('mongoose');
 const Route=express.Router();
 const Document=require("../model/Documents")
+const multer=require('multer')
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,'./Upload/')
+    },
+    filename:(req,file,cb)=>{
+        cb(null, new Date().toString()+file.originalname)
+        // cb(null, new Date().toString() + file.originalname);
+    }
+})
+const Upload=multer({storage:storage})
 Route.post('/',(req,res,next)=>{
     // res.s
     if(req.body.Category=="ThreeWords"||req.body.Category=="FiveSentence"){
@@ -42,16 +53,16 @@ Route.post('/',(req,res,next)=>{
         })
     }
 })
-Route.patch('/',(req,res,next)=>{
+Route.patch('/',Upload.single('Upload'),(req,res,next)=>{
     const document=new Document({
         _id:mongoose.Types.ObjectId(),
         Category:req.body.Category,
         Name:req.body.Name,
-        PronunSymbol:req.body.PronunSymbol
+        VocalImage:req.file.path
     })
     document.save().then(()=>{
         res.status(200).json({
-            message:"Okey Data Captured"
+            message:"Data Captured"
         })
     }).catch((err)=>{
         res.status(300).json({
